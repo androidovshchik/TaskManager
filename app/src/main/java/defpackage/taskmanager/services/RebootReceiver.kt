aca3b5027780e556c4325a623ebf89971b92d2e4
vladislav.k <vladislav.k@appstockus.com>
2019-09-06 17:00:03 +0300

    changes

---
+++ app/src/main/java/defpackage/taskmanager/services/RebootReceiver.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019. Vlad Kalyuzhnyu <vladkalyuzhnyu@gmail.com>
+ */
+
+package defpackage.taskmanager.services
+
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class RebootReceiver : BroadcastReceiver() {
+
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
+    override fun onReceive(context: Context, intent: Intent) {
+
+    }
+}
---
+++ app/src/main/java/defpackage/taskmanager/services/TaskReceiver.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2019. Vlad Kalyuzhnyu <vladkalyuzhnyu@gmail.com>
+ */
+
+package defpackage.taskmanager.services
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class TaskReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+    }
+}
