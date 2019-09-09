b284dd1bfcb76b555c47ab73a45fe462bd2cf330
vladislav.k <vladislav.k@appstockus.com>
2019-09-09 12:52:17 +0300

    changes

---
+++ app/src/main/java/defpackage/taskmanager/receivers/TasksWidget.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2019. Vlad Kalyuzhnyu <vladkalyuzhnyu@gmail.com>
+ */
+
+package defpackage.taskmanager.receivers
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class TasksWidget : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+
+    }
+}
